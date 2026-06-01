add67f7266128596ee9a15810b202a7572a9cc7c
Wynton de Kort <wyntontjes123@outlook.com>
2026-06-01 13:40:39 +0200

    Working towards v3 (Wip 2)

--- rooster.ps1
+++ rooster.ps1
@@ -155,13 +155,13 @@ Function New-Table {
 		foreach ($day in @("Ma", "Di", "Wo", "Do", "Vr")) {
 			if ($Days.ContainsKey($day) -and $Days[$day].Count -ge $hour) {
 				if ($($Days[$day][$hour - 1]).Length -eq 3) {
-					$row += "   $($Days[$day][$hour - 1])  │"
+					$row += "  $($Days[$day][$hour - 1])   │"
 				}
 				elseif ($($Days[$day][$hour - 1]).Length -eq 4) {
-					$row += "   $($Days[$day][$hour - 1]) │"
+					$row += "  $($Days[$day][$hour - 1])  │"
 				}
 				else {
-					$row += "   $($Days[$day][$hour - 1])│"
+					$row += "  $($Days[$day][$hour - 1]) │"
 				}
 			}
 			else {
